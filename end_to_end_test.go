f0a4c664eb8d3edb3eabaca578e06a09ad3c2649
oliverpool <git@olivier.pfad.fr>
2022-06-01 16:58:48 +0200

    init test

--- end_to_end_test.go
+++ end_to_end_test.go
@@ -1,7 +1,35 @@
 package risefront
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	switch os.Getenv("RISEFRONT_TEST_MODE") {
+	default:
+		// Normal test mode
+		os.Exit(m.Run())
+
+	case "run":
+		// TODO call equivalent of main
+		fmt.Println("run", os.Getpid())
+	}
+}
+
+func runSelf(mode string, arg ...string) ([]byte, error) {
+	cmd := exec.Command(os.Args[0], arg...)
+	cmd.Env = []string{"RISEFRONT_TEST_MODE=" + mode}
+	return cmd.CombinedOutput()
+}
 
 func TestEndToEnd(t *testing.T) {
-	t.Log("ok")
+	// go func() {
+	out, err := runSelf("run", "lol")
+	t.Log(string(out), err)
+	// }()
+	t.Log("ok", os.Getpid())
+	t.Fail()
 }
